dfdaa48cea5e2e32ea3e10e64d394a20c11e4eb6
Corinna Grobe <corinna.grobe@statistik.ji.zh.ch>
2021-03-31 09:23:28 +0200

    Updating packageVersions and removing meteo_schweiz.RDS

--- set-up.R
+++ set-up.R
@@ -11,19 +11,19 @@ require(rOstluft) # Version ‘1.4.0’
 require(rOstluft.plot)
 require(data.table) #Version ‘1.13.4’
 require(readr) # Version ‘1.3.1’
-require(tidyverse)
+require(tidyverse) # Version ‘1.3.0’
 require(dplyr) # Verion ‘1.0.0’
 require(tidyr) # Version ‘1.1.0’
 require(lubridate) # Version ‘1.7.9’
 require(ggplot2) # Version ‘3.3.2’
-require(plotly)
+require(plotly) # Version ‘4.9.3’
 require(statR) # Version ‘0.0.0.9000’
-require(sp)
-require(ggmap)
-require(tmaptools)
-require(leaflet)
-require(htmlTable)
-require(DT)
+require(sp) # Version ‘1.4.4’
+require(ggmap) # ‘3.0.0’
+require(tmaptools) # Version ‘3.1’
+require(leaflet) # Version ‘2.0.3’
+require(htmlTable) # Version ‘2.1.0’
+require(DT) # Version ‘0.17’
 
 
 
